32af19af3e529b9fae4c7a9f89d7a4967b987252
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-02 14:52:57 -0700

    indexdb functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,10 +16,10 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // check if app is online, if yes run uploadBudget() function to send all local db data to api
     if (navigator.onLine) {
       // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadPizza();
+      // uploadBudget();
     }
   };
   
