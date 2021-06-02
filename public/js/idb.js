06fb9e6548260f45a56e3b4c12afdc6da5f776d6
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-02 14:52:11 -0700

    indexdb functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,7 +7,24 @@ const request = indexedDB.open('budget-tracker', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_pizza', { autoIncrement: true });
+    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_budget', { autoIncrement: true });
+  };
+
+  // upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    if (navigator.onLine) {
+      // we haven't created this yet, but we will soon, so let's comment it out for now
+      // uploadPizza();
+    }
+  };
+  
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
   };
   
