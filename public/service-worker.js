50ca4fd9f686d4768050f946ed1b53b8f00dd855
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-03 14:10:36 -0700

    indexdb functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,27 @@ const FILES_TO_CACHE = [
 
   ];
 
-self.addEventListener('install', function (e) {
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
+  
+  // Cache resources
+  self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
         console.log('installing cache : ' + CACHE_NAME)
@@ -20,3 +40,27 @@ self.addEventListener('install', function (e) {
       })
     )
   })
+  
+  // Delete outdated caches
+  self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        // `keyList` contains all cache names under your username.github.io
+        // filter out ones that has this app prefix to create keeplist
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        // add current cache name to keeplist
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+  });
