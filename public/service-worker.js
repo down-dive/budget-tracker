61e9358e256883eea3aef22a38a993cad5abfbd4
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-03 14:09:50 -0700

    indexdb functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,16 @@ const APP_PREFIX = 'TransactionEvent-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./js/idb.js",
+    "./js/index.js",
+
+  ];
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
