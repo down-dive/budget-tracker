78991571c6bd310f7e9d5c25d5bf6112a8dd7c90
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-03 14:05:51 -0700

    indexdb functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', function (e) {
-
-})
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
